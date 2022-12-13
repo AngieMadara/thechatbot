eebf154018c7fe3325ba6cb09ac5438622c925e6
Chris Carmichael <chris@chriscarm.co>
2022-12-13 03:15:22 -0800

    server setup

---
+++ bot.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+from random import choice
+from flask import Flask, request 
+import os
+import openai
+
+load_dotenv()
+#openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "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"
+completion = openai.Completion()
+
+start_sequence = "\nBot:"
+restart_sequence = "\n\nPerson:"
+session_prompt = "You are talking to Bot, GPT3 bot. \n\nPerson:"
+
+def ask(question, chat_log=None):
+    prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
+    response = openai.Completion.create(
+      engine="text-davinci-003",
+    #   model="text-davinci-003",
+      prompt=prompt_text,
+      temperature=0.8,
+      max_tokens=150,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0.3,
+      stop=["\n"],
+    )
+    story = response['choices'][0]['text']
+    return str(story)
+
+def append_interaction_to_chat_log(question, answer, chat_log=None):
+    if chat_log is None:
+        chat_log = session_prompt
+    return f'{chat_log}{restart_sequence} {question}{start_sequence}{answer}'
