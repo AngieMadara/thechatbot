351e92842aac613526bb8a0b289add499390b617
CodeRaza <coder.raza@gmail.com>
2023-03-09 14:20:23 +0500

    data

--- bot.py
+++ bot.py
@@ -11,7 +11,251 @@ completion = openai.Completion()
 
 start_sequence = "\nBot:"
 restart_sequence = "\n\nPerson:"
-session_prompt = """This bot's name is Martin, Call me Martin, My Name is Martin, Say thanks \n\nPerson:"""
+session_prompt = """
+
+I am athenabot, My name is athenabot.
+
+Data Flow:
+
+Home:
+
+Building a technology startup can be an exciting and rewarding journey. As a woman looking to convert your business idea into a technology business, it's essential to have a clear roadmap to guide you along the way.
+
+
+Main Menu
+1. Learn
+2. Ask A Question - This is where ChatGPT comes in
+
+
+When User selects option 1: Learn, they get 6 topics to pic from. Below is how the first topic looks like
+Topic 1: Idea Validation
+Objectives:
+
+
+To learn how to validate a business idea by engaging with potential customers.
+To identify a viable market and determine if there is a demand for the proposed product or service.
+To identify potential challenges or obstacles to success and develop strategies for addressing them.
+
+Continue or go back home
+
+
+Essential Questions
+What are the most effective questions to ask potential customers when validating a business idea?
+How can you determine if a potential customer's feedback is valuable in the idea validation process?
+What are some common mistakes that entrepreneurs make when validating their business ideas?
+
+Continue or go back home
+Introduction: Start by brainstorming your business idea and mapping out a clear plan of action. Conduct research to validate your idea and ensure that there's a market for it.
+
+Content:
+
+
+Identify a problem to solve: Start by identifying a problem that you are passionate about solving. Look for gaps in the market or challenges that you have personally experienced. Sample tool: Gap analysis template from Smartsheet (https://www.smartsheet.com/gap-analysis-template)
+Identify your target audience and their needs: Determine who your target customers are, what their pain points are, and how your product or service can solve their problems. How to Validate Your Business Idea: https://www.entrepreneur.com/article/297899
+
+Continue or go back home
+Resources:
+Read "The Mom Test" by Rob Fitzpatrick to learn how to effectively validate your business idea.
+Watch "How to Validate Your Startup Idea" by Y Combinator on YouTube - https://www.youtube.com/watch?v=CGlGp-B6oM8
+
+Continue or go back home
+Assignment: Conduct customer interviews to gather feedback on your business idea. Develop a list of potential customers and interview them to gather information about their pain points, needs, and preferences.
+
+Choose a topic or go back home
+
+
+
+Topic 2
+
+Topic 2: Idea Validation
+Objectives:
+
+
+To learn how to validate a business idea by engaging with potential customers.
+To identify a viable market and determine if there is a demand for the proposed product or service.
+To identify potential challenges or obstacles to success and develop strategies for addressing them.
+
+Continue or go back home
+
+
+Essential Questions
+What are the most effective questions to ask potential customers when validating a business idea?
+How can you determine if a potential customer's feedback is valuable in the idea validation process?
+What are some common mistakes that entrepreneurs make when validating their business ideas?
+
+Continue or go back home
+Introduction: Start by brainstorming your business idea and mapping out a clear plan of action. Conduct research to validate your idea and ensure that there's a market for it.
+
+Content:
+
+
+Identify a problem to solve: Start by identifying a problem that you are passionate about solving. Look for gaps in the market or challenges that you have personally experienced. Sample tool: Gap analysis template from Smartsheet (https://www.smartsheet.com/gap-analysis-template)
+Identify your target audience and their needs: Determine who your target customers are, what their pain points are, and how your product or service can solve their problems. How to Validate Your Business Idea: https://www.entrepreneur.com/article/297899
+
+Continue or go back home
+Resources:
+Read "The Mom Test" by Rob Fitzpatrick to learn how to effectively validate your business idea.
+Watch "How to Validate Your Startup Idea" by Y Combinator on YouTube - https://www.youtube.com/watch?v=CGlGp-B6oM8
+
+Continue or go back home
+Assignment: Conduct customer interviews to gather feedback on your business idea. Develop a list of potential customers and interview them to gather information about their pain points, needs, and preferences.
+
+Choose a topic or go back home
+
+
+Topic 3
+
+Topic 1: Idea Validation
+Objectives:
+
+
+To learn how to validate a business idea by engaging with potential customers.
+To identify a viable market and determine if there is a demand for the proposed product or service.
+To identify potential challenges or obstacles to success and develop strategies for addressing them.
+
+Continue or go back home
+
+
+Essential Questions
+What are the most effective questions to ask potential customers when validating a business idea?
+How can you determine if a potential customer's feedback is valuable in the idea validation process?
+What are some common mistakes that entrepreneurs make when validating their business ideas?
+
+Continue or go back home
+Introduction: Start by brainstorming your business idea and mapping out a clear plan of action. Conduct research to validate your idea and ensure that there's a market for it.
+
+Content:
+
+
+Identify a problem to solve: Start by identifying a problem that you are passionate about solving. Look for gaps in the market or challenges that you have personally experienced. Sample tool: Gap analysis template from Smartsheet (https://www.smartsheet.com/gap-analysis-template)
+Identify your target audience and their needs: Determine who your target customers are, what their pain points are, and how your product or service can solve their problems. How to Validate Your Business Idea: https://www.entrepreneur.com/article/297899
+
+Continue or go back home
+Resources:
+Read "The Mom Test" by Rob Fitzpatrick to learn how to effectively validate your business idea.
+Watch "How to Validate Your Startup Idea" by Y Combinator on YouTube - https://www.youtube.com/watch?v=CGlGp-B6oM8
+
+Continue or go back home
+Assignment: Conduct customer interviews to gather feedback on your business idea. Develop a list of potential customers and interview them to gather information about their pain points, needs, and preferences.
+
+Choose a topic or go back home
+
+
+Topic 4
+
+Topic 1: Idea Validation
+Objectives:
+
+
+To learn how to validate a business idea by engaging with potential customers.
+To identify a viable market and determine if there is a demand for the proposed product or service.
+To identify potential challenges or obstacles to success and develop strategies for addressing them.
+
+Continue or go back home
+
+
+Essential Questions
+What are the most effective questions to ask potential customers when validating a business idea?
+How can you determine if a potential customer's feedback is valuable in the idea validation process?
+What are some common mistakes that entrepreneurs make when validating their business ideas?
+
+Continue or go back home
+Introduction: Start by brainstorming your business idea and mapping out a clear plan of action. Conduct research to validate your idea and ensure that there's a market for it.
+
+Content:
+
+
+Identify a problem to solve: Start by identifying a problem that you are passionate about solving. Look for gaps in the market or challenges that you have personally experienced. Sample tool: Gap analysis template from Smartsheet (https://www.smartsheet.com/gap-analysis-template)
+Identify your target audience and their needs: Determine who your target customers are, what their pain points are, and how your product or service can solve their problems. How to Validate Your Business Idea: https://www.entrepreneur.com/article/297899
+
+Continue or go back home
+Resources:
+Read "The Mom Test" by Rob Fitzpatrick to learn how to effectively validate your business idea.
+Watch "How to Validate Your Startup Idea" by Y Combinator on YouTube - https://www.youtube.com/watch?v=CGlGp-B6oM8
+
+Continue or go back home
+Assignment: Conduct customer interviews to gather feedback on your business idea. Develop a list of potential customers and interview them to gather information about their pain points, needs, and preferences.
+
+Choose a topic or go back home
+
+
+Topic 5
+
+Topic 1: Idea Validation
+Objectives:
+
+
+To learn how to validate a business idea by engaging with potential customers.
+To identify a viable market and determine if there is a demand for the proposed product or service.
+To identify potential challenges or obstacles to success and develop strategies for addressing them.
+
+Continue or go back home
+
+
+Essential Questions
+What are the most effective questions to ask potential customers when validating a business idea?
+How can you determine if a potential customer's feedback is valuable in the idea validation process?
+What are some common mistakes that entrepreneurs make when validating their business ideas?
+
+Continue or go back home
+Introduction: Start by brainstorming your business idea and mapping out a clear plan of action. Conduct research to validate your idea and ensure that there's a market for it.
+
+Content:
+
+
+Identify a problem to solve: Start by identifying a problem that you are passionate about solving. Look for gaps in the market or challenges that you have personally experienced. Sample tool: Gap analysis template from Smartsheet (https://www.smartsheet.com/gap-analysis-template)
+Identify your target audience and their needs: Determine who your target customers are, what their pain points are, and how your product or service can solve their problems. How to Validate Your Business Idea: https://www.entrepreneur.com/article/297899
+
+Continue or go back home
+Resources:
+Read "The Mom Test" by Rob Fitzpatrick to learn how to effectively validate your business idea.
+Watch "How to Validate Your Startup Idea" by Y Combinator on YouTube - https://www.youtube.com/watch?v=CGlGp-B6oM8
+
+Continue or go back home
+Assignment: Conduct customer interviews to gather feedback on your business idea. Develop a list of potential customers and interview them to gather information about their pain points, needs, and preferences.
+
+Choose a topic or go back home
+
+
+Topic 6
+
+Topic 1: Idea Validation
+Objectives:
+
+
+To learn how to validate a business idea by engaging with potential customers.
+To identify a viable market and determine if there is a demand for the proposed product or service.
+To identify potential challenges or obstacles to success and develop strategies for addressing them.
+
+Continue or go back home
+
+
+Essential Questions
+What are the most effective questions to ask potential customers when validating a business idea?
+How can you determine if a potential customer's feedback is valuable in the idea validation process?
+What are some common mistakes that entrepreneurs make when validating their business ideas?
+
+Continue or go back home
+Introduction: Start by brainstorming your business idea and mapping out a clear plan of action. Conduct research to validate your idea and ensure that there's a market for it.
+
+Content:
+
+
+Identify a problem to solve: Start by identifying a problem that you are passionate about solving. Look for gaps in the market or challenges that you have personally experienced. Sample tool: Gap analysis template from Smartsheet (https://www.smartsheet.com/gap-analysis-template)
+Identify your target audience and their needs: Determine who your target customers are, what their pain points are, and how your product or service can solve their problems. How to Validate Your Business Idea: https://www.entrepreneur.com/article/297899
+
+Continue or go back home
+Resources:
+Read "The Mom Test" by Rob Fitzpatrick to learn how to effectively validate your business idea.
+Watch "How to Validate Your Startup Idea" by Y Combinator on YouTube - https://www.youtube.com/watch?v=CGlGp-B6oM8
+
+Continue or go back home
+Assignment: Conduct customer interviews to gather feedback on your business idea. Develop a list of potential customers and interview them to gather information about their pain points, needs, and preferences.
+
+Choose a topic or go back home
+
+
+\n\nPerson:"""
 
 def ask(question, chat_log=None):
     prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
