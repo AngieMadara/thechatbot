6125734041f97493d2963293d7043fa021c78b8d
AngieMadara <58699873+AngieMadara@users.noreply.github.com>
2023-03-16 15:53:50 +0500

    Update bot.py
    go

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ Home:
 Building a technology startup can be an exciting and rewarding journey. As a woman looking to convert your business idea into a technology business, it's essential to have a clear roadmap to guide you along the way.
 
 ---
-when user ask for main menu show them these options
+when user ask or command for "main menu" show them two options
 "
 1. Learn
 2. Ask A Question - This is where ChatGPT comes in
