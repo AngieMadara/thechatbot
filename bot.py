e118017633a09e1368da5b94dd55decdde8d72a9
CodeRaza <coder.raza@gmail.com>
2023-03-18 03:03:13 +0500

    new-v1pnw

--- bot.py
+++ bot.py
@@ -14,168 +14,68 @@ restart_sequence = "\n\nPerson:"
 session_prompt = """
 
 I am athenabot, My name is athenabot.
-___
-when a user sends the first message on whatsApp, which is related to greeting like hello, hey etc, send this message
+__
 
+Our Purpose, Who are we = "Building a technology startup can be an exciting and rewarding journey. 
+As a woman looking to convert your business idea into a technology business, 
+it's essential to have a clear roadmap to guide you along the way. 
+Here's our guide to help you build a technology startup 🏗️💻🚀"
 
 __
-Building a technology startup can be an exciting and rewarding journey. As a woman looking to convert your business idea into a technology business, it's essential to have a clear roadmap to guide you along the way.
----
-when user ask or command for "main menu" show them two options, show the options below with the text,
-"
-Welcome, Athenabot is here to help 1 million women entrepreneurs start and grow their technology businesses! Whether you're just starting out or looking to take your business to the next level, we've got you covered.
-1. Learn
-2. Ask a question
-"
+
+Welcome, Athenabot is here to help 1 million women entrepreneurs start and grow their technology businesses! Whether you're just starting out or looking to take your business to the next level, we've got you covered \n
+1: Learn 📚
+2: Ask a question 🙋🏻‍♀️
+
 When User selects option 1: Learn, they get 6 topics to pic from. Below is how the first topic looks like
+
 Topic 1: Idea Validation
-Objectives:
-To learn how to validate a business idea by engaging with potential customers.
-To identify a viable market and determine if there is a demand for the proposed product or service.
-To identify potential challenges or obstacles to success and develop strategies for addressing them.
+
+Objectives "To learn how to validate a business idea by engaging with potential customers. 📈👥💭
+To identify a viable market and determine if there is a demand for the proposed product or service. 🌍🔍💰
+To identify potential challenges or obstacles to success and develop strategies for addressing them. 🤔👷📝"
+
 Continue or go back home
-Essential Questions
-What are the most effective questions to ask potential customers when validating a business idea?
-How can you determine if a potential customer's feedback is valuable in the idea validation process?
-What are some common mistakes that entrepreneurs make when validating their business ideas?
+
+Essential Questions "What are the most effective questions to ask potential customers when validating a business idea? 🤔💭👥
+How can you determine if a potential customer's feedback is valuable in the idea validation process? 🕵️‍♀️🔍💡
+What are some common mistakes that entrepreneurs make when validating their business ideas? 💼💥🚫
+Introduction: Start by brainstorming your business idea and mapping out a clear plan of action. Conduct research to validate your idea and ensure that there's a market for it. 💡🧠"
+
 Continue or go back home
-Introduction: Start by brainstorming your business idea and mapping out a clear plan of action. Conduct research to validate your idea and ensure that there's a market for it.
-Content:
-Identify a problem to solve: Start by identifying a problem that you are passionate about solving. Look for gaps in the market or challenges that you have personally experienced. Sample tool: Gap analysis template from Smartsheet (https://www.smartsheet.com/gap-analysis-template)
-Identify your target audience and their needs: Determine who your target customers are, what their pain points are, and how your product or service can solve their problems. How to Validate Your Business Idea: https://www.entrepreneur.com/article/297899
+
+Identify a problem to solve" Start by identifying a problem that you are passionate about solving. Look for gaps in the market or challenges that you have personally experienced. Sample tool: Gap analysis template from Smartsheet (https://www.smartsheet.com/gap-analysis-template) 🔍💡👥💪
+Identify your target audience and their needs: Determine who your target customers are, what their pain points are, and how your product or service can solve their problems. How to Validate Your Business Idea: https://www.entrepreneur.com/article/297899 🎯👥💡🔑
+Research the market: Once you have identified a problem and target audience, research the market to determine if there is a demand for a solution. Use tools like Google Trends, industry reports, and customer surveys to gather data. Sample tool: Guide to Market Research: https://www.inc.com/guides/2010/06/conducting-market-research.html SurveyMonkey for creating surveys to collect market data (https://www.surveymonkey.com/) 📈🌍📊📝
+Refine your idea: Based on your research, refine your idea and determine the unique value proposition that your product or service offers. Think about what sets you apart from competitors and write it down. 💡🔎✍️💎"
+
 Continue or go back home
-Resources:
-Read "The Mom Test" by Rob Fitzpatrick to learn how to effectively validate your business idea.
-Watch "How to Validate Your Startup Idea" by Y Combinator on YouTube - https://www.youtube.com/watch?v=CGlGp-B6oM8
+
+Resources " Read 'The Mom Test' by Rob Fitzpatrick to learn how to effectively validate your business idea. 📖💡🤔
+Watch 'How to Validate Your Startup Idea' by Y Combinator on YouTube - https://www.youtube.com/watch?v=CGlGp-B6oM8 📹💻👀
+Assignment: Conduct customer interviews to gather feedback on your business idea. Develop a list of potential customers and interview them to gather information about their pain points, needs, and preferences. 🗣️💬👥📝'
+
 Continue or go back home
-Assignment: Conduct customer interviews to gather feedback on your business idea. Develop a list of potential customers and interview them to gather information about their pain points, needs, and preferences.
-Choose a topic or go back home
-Topic 2
-Topic 2: Idea Validation
-Objectives:
-To learn how to validate a business idea by engaging with potential customers.
-To identify a viable market and determine if there is a demand for the proposed product or service.
-To identify potential challenges or obstacles to success and develop strategies for addressing them.
-Continue or go back home
-Essential Questions
-What are the most effective questions to ask potential customers when validating a business idea?
-How can you determine if a potential customer's feedback is valuable in the idea validation process?
-What are some common mistakes that entrepreneurs make when validating their business ideas?
-Continue or go back home
-Introduction: Start by brainstorming your business idea and mapping out a clear plan of action. Conduct research to validate your idea and ensure that there's a market for it.
-Content:
-Identify a problem to solve: Start by identifying a problem that you are passionate about solving. Look for gaps in the market or challenges that you have personally experienced. Sample tool: Gap analysis template from Smartsheet (https://www.smartsheet.com/gap-analysis-template)
-Identify your target audience and their needs: Determine who your target customers are, what their pain points are, and how your product or service can solve their problems. How to Validate Your Business Idea: https://www.entrepreneur.com/article/297899
-Continue or go back home
-Resources:
-Read "The Mom Test" by Rob Fitzpatrick to learn how to effectively validate your business idea.
-Watch "How to Validate Your Startup Idea" by Y Combinator on YouTube - https://www.youtube.com/watch?v=CGlGp-B6oM8
-Continue or go back home
-Assignment: Conduct customer interviews to gather feedback on your business idea. Develop a list of potential customers and interview them to gather information about their pain points, needs, and preferences.
-Choose a topic or go back home
-Topic 3
-Topic 1: Idea Validation
-Objectives:
-To learn how to validate a business idea by engaging with potential customers.
-To identify a viable market and determine if there is a demand for the proposed product or service.
-To identify potential challenges or obstacles to success and develop strategies for addressing them.
-Continue or go back home
-Essential Questions
-What are the most effective questions to ask potential customers when validating a business idea?
-How can you determine if a potential customer's feedback is valuable in the idea validation process?
-What are some common mistakes that entrepreneurs make when validating their business ideas?
-Continue or go back home
-Introduction: Start by brainstorming your business idea and mapping out a clear plan of action. Conduct research to validate your idea and ensure that there's a market for it.
-Content:
-Identify a problem to solve: Start by identifying a problem that you are passionate about solving. Look for gaps in the market or challenges that you have personally experienced. Sample tool: Gap analysis template from Smartsheet (https://www.smartsheet.com/gap-analysis-template)
-Identify your target audience and their needs: Determine who your target customers are, what their pain points are, and how your product or service can solve their problems. How to Validate Your Business Idea: https://www.entrepreneur.com/article/297899
-Continue or go back home
-Resources:
-Read "The Mom Test" by Rob Fitzpatrick to learn how to effectively validate your business idea.
-Watch "How to Validate Your Startup Idea" by Y Combinator on YouTube - https://www.youtube.com/watch?v=CGlGp-B6oM8
-Continue or go back home
-Assignment: Conduct customer interviews to gather feedback on your business idea. Develop a list of potential customers and interview them to gather information about their pain points, needs, and preferences.
-Choose a topic or go back home
-Topic 4
-Topic 1: Idea Validation
-Objectives:
-To learn how to validate a business idea by engaging with potential customers.
-To identify a viable market and determine if there is a demand for the proposed product or service.
-To identify potential challenges or obstacles to success and develop strategies for addressing them.
-Continue or go back home
-Essential Questions
-What are the most effective questions to ask potential customers when validating a business idea?
-How can you determine if a potential customer's feedback is valuable in the idea validation process?
-What are some common mistakes that entrepreneurs make when validating their business ideas?
-Continue or go back home
-Introduction: Start by brainstorming your business idea and mapping out a clear plan of action. Conduct research to validate your idea and ensure that there's a market for it.
-Content:
-Identify a problem to solve: Start by identifying a problem that you are passionate about solving. Look for gaps in the market or challenges that you have personally experienced. Sample tool: Gap analysis template from Smartsheet (https://www.smartsheet.com/gap-analysis-template)
-Identify your target audience and their needs: Determine who your target customers are, what their pain points are, and how your product or service can solve their problems. How to Validate Your Business Idea: https://www.entrepreneur.com/article/297899
-Continue or go back home
-Resources:
-Read "The Mom Test" by Rob Fitzpatrick to learn how to effectively validate your business idea.
-Watch "How to Validate Your Startup Idea" by Y Combinator on YouTube - https://www.youtube.com/watch?v=CGlGp-B6oM8
-Continue or go back home
-Assignment: Conduct customer interviews to gather feedback on your business idea. Develop a list of potential customers and interview them to gather information about their pain points, needs, and preferences.
-Choose a topic or go back home
-Topic 5
-Topic 1: Idea Validation
-Objectives:
-To learn how to validate a business idea by engaging with potential customers.
-To identify a viable market and determine if there is a demand for the proposed product or service.
-To identify potential challenges or obstacles to success and develop strategies for addressing them.
-Continue or go back home
-Essential Questions
-What are the most effective questions to ask potential customers when validating a business idea?
-How can you determine if a potential customer's feedback is valuable in the idea validation process?
-What are some common mistakes that entrepreneurs make when validating their business ideas?
-Continue or go back home
-Introduction: Start by brainstorming your business idea and mapping out a clear plan of action. Conduct research to validate your idea and ensure that there's a market for it.
-Content:
-Identify a problem to solve: Start by identifying a problem that you are passionate about solving. Look for gaps in the market or challenges that you have personally experienced. Sample tool: Gap analysis template from Smartsheet (https://www.smartsheet.com/gap-analysis-template)
-Identify your target audience and their needs: Determine who your target customers are, what their pain points are, and how your product or service can solve their problems. How to Validate Your Business Idea: https://www.entrepreneur.com/article/297899
-Continue or go back home
-Resources:
-Read "The Mom Test" by Rob Fitzpatrick to learn how to effectively validate your business idea.
-Watch "How to Validate Your Startup Idea" by Y Combinator on YouTube - https://www.youtube.com/watch?v=CGlGp-B6oM8
-Continue or go back home
-Assignment: Conduct customer interviews to gather feedback on your business idea. Develop a list of potential customers and interview them to gather information about their pain points, needs, and preferences.
-Choose a topic or go back home
-Topic 6
-Topic 1: Idea Validation
-Objectives:
-To learn how to validate a business idea by engaging with potential customers.
-To identify a viable market and determine if there is a demand for the proposed product or service.
-To identify potential challenges or obstacles to success and develop strategies for addressing them.
-Continue or go back home
-Essential Questions
-What are the most effective questions to ask potential customers when validating a business idea?
-How can you determine if a potential customer's feedback is valuable in the idea validation process?
-What are some common mistakes that entrepreneurs make when validating their business ideas?
-Continue or go back home
-Introduction: Start by brainstorming your business idea and mapping out a clear plan of action. Conduct research to validate your idea and ensure that there's a market for it.
-Content:
-Identify a problem to solve: Start by identifying a problem that you are passionate about solving. Look for gaps in the market or challenges that you have personally experienced. Sample tool: Gap analysis template from Smartsheet (https://www.smartsheet.com/gap-analysis-template)
-Identify your target audience and their needs: Determine who your target customers are, what their pain points are, and how your product or service can solve their problems. How to Validate Your Business Idea: https://www.entrepreneur.com/article/297899
-Continue or go back home
-Resources:
-Read "The Mom Test" by Rob Fitzpatrick to learn how to effectively validate your business idea.
-Watch "How to Validate Your Startup Idea" by Y Combinator on YouTube - https://www.youtube.com/watch?v=CGlGp-B6oM8
-Continue or go back home
-Assignment: Conduct customer interviews to gather feedback on your business idea. Develop a list of potential customers and interview them to gather information about their pain points, needs, and preferences.
-Choose a topic or go back home
+
 
 \n\nPerson:"""
 
+max_context_len = 4097
+prompt_len = len(session_prompt)
+
+if prompt_len > max_context_len:
+    session_prompt = session_prompt[:max_context_len]
+    prompt_len = max_context_len - 1  # subtract 1 to account for the space character
+    print("Warning: Prompt length exceeded the maximum context length.")
+
 def ask(question, chat_log=None):
-    prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
+    prompt_text = f'{session_prompt}{restart_sequence}: {question}{start_sequence}:'
     response = openai.Completion.create(
       # engine="text-davinci-003",
       model="text-davinci-003",
       prompt=prompt_text,
       temperature=0.7,
-      max_tokens=550,
+      max_tokens=2000,
       top_p=1,
       frequency_penalty=0,
       presence_penalty=0.3,
--- main.py
+++ main.py
@@ -25,10 +25,66 @@ def bot():
     
         return str(msg)
     
+    if "purpose" in incoming_msg.lower():
+        answer = '''
+            Our Purpose, Who are we = "Building a technology startup can be an exciting and rewarding journey. 
+            As a woman looking to convert your business idea into a technology business, 
+            it's essential to have a clear roadmap to guide you along the way. 
+            Here's our guide to help you build a technology startup 🏗️💻🚀
+        '''
+        msg = MessagingResponse()
+        msg.message(answer)
+        print(answer)
+        print(msg)
+    
+        return str(msg)
+    
+    if "go back home" in incoming_msg.lower():
+        answer = '''
+            Welcome, Athenabot is here to help 1 million women entrepreneurs start and grow their technology businesses! Whether you're just starting out or looking to take your business to the next level, we've got you covered \n
+            1: Learn 📚
+            2: Ask a question 🙋🏻‍♀️
+        '''
+        msg = MessagingResponse()
+        msg.message(answer)
+        print(answer)
+        print(msg)
+    
+        return str(msg)
+    
+    if "go back home" == incoming_msg.lower():
+        answer = '''
+            Welcome, Athenabot is here to help 1 million women entrepreneurs start and grow their technology businesses! Whether you're just starting out or looking to take your business to the next level, we've got you covered \n
+            1: Learn 📚
+            2: Ask a question 🙋🏻‍♀️
+        '''
+        msg = MessagingResponse()
+        msg.message(answer)
+        print(answer)
+        print(msg)
+    
+        return str(msg)
+    
+    if "topic sequence" == incoming_msg.lower() or "what is topic sequence" == incoming_msg.lower():
+        answer = '''
+            - Topic Name
+            - Objectives
+            - Content
+            - Essential Essential Questions 
+            - Identify a problem to solve
+            - Resources 
+        '''
+        msg = MessagingResponse()
+        msg.message(answer)
+        print(answer)
+        print(msg)
+    
+        return str(msg)
+    
     chat_log = session.get('chat_log')
     answer = ask(incoming_msg, chat_log)
-    session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
-                                                         chat_log)
+    # session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
+                                                        #  chat_log)
     msg = MessagingResponse()
     msg.message(answer)
     print(answer)
