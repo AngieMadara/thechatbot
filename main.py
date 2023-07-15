e9b2bd1228a3c8a1f23a979e00ea1e7bbe5acf23
Ali Raza <coder.raza@gmail.com>
2023-07-16 00:17:36 +0500

    Changes

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from flask import Flask, request, session, Response
 from twilio.twiml.messaging_response import MessagingResponse
-from bot import ask, append_interaction_to_chat_log
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
 # if for some reason your conversation with the bot gets weird, change the secret key 
-app.config['SECRET_KEY'] = 'nd23hc823hc0923h3j2nc0393d2d23'
+app.config['SECRET_KEY'] = 'nd23hc823hcbhjgug7g8g89bkh70923h3j2nc0393d2d23'
 
 @app.route('/bot', methods=['POST'])
 def bot():
@@ -20,7 +18,6 @@ def bot():
 @app.route('/test', methods=['POST'])
 def test():
     incoming_msg = request.values['Body']
-   
     return incoming_msg
 
 if __name__ == '__main__':
