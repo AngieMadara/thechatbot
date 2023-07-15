5bd1dcbfc86fd6f990a6a82da705f8c7c2fd103c
Ali Raza <coder.raza@gmail.com>
2023-07-15 16:08:03 +0500

    LangChain Solution

--- main.py
+++ main.py
@@ -1,111 +1,19 @@
 from flask import Flask, request, session, Response
 from twilio.twiml.messaging_response import MessagingResponse
 from bot import ask, append_interaction_to_chat_log
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 # if for some reason your conversation with the bot gets weird, change the secret key 
 app.config['SECRET_KEY'] = '8wLs1T3BlbkFJUJ5BxAcvjbvkjgffvrOjUcAMX7njo7vjvhj8nmb'
 
-
-
 @app.route('/bot', methods=['POST'])
 def bot():
     incoming_msg = request.values['Body']
-    
-    if "Main Menu" in incoming_msg or "menu" in incoming_msg or "main menu" in incoming_msg:
-        answer = '''
-            Welcome, Athenabot is here to help 1 million women entrepreneurs start and grow their technology businesses! Whether you're just starting out or looking to take your business to the next level, we've got you covered \n
-            1: Learn 📚
-            2: Ask a question 🙋🏻‍♀️
-        '''
-        msg = MessagingResponse()
-        msg.message(answer)
-        print(answer)
-        print(msg)
-    
-        return str(msg)
-    
-    if "purpose" in incoming_msg.lower():
-        answer = '''
-            Our Purpose, Who are we = "Building a technology startup can be an exciting and rewarding journey. 
-            As a woman looking to convert your business idea into a technology business, 
-            it's essential to have a clear roadmap to guide you along the way. 
-            Here's our guide to help you build a technology startup 🏗️💻🚀
-        '''
-        msg = MessagingResponse()
-        msg.message(answer)
-        print(answer)
-        print(msg)
-    
-        return str(msg)
-    
-    if "go back home" in incoming_msg.lower():
-        answer = '''
-            Welcome, Athenabot is here to help 1 million women entrepreneurs start and grow their technology businesses! Whether you're just starting out or looking to take your business to the next level, we've got you covered \n
-            1: Learn 📚
-            2: Ask a question 🙋🏻‍♀️
-        '''
-        msg = MessagingResponse()
-        msg.message(answer)
-        print(answer)
-        print(msg)
-    
-        return str(msg)
-    
-    if "go back home" == incoming_msg.lower():
-        answer = '''
-            Welcome, Athenabot is here to help 1 million women entrepreneurs start and grow their technology businesses! Whether you're just starting out or looking to take your business to the next level, we've got you covered \n
-            1: Learn 📚
-            2: Ask a question 🙋🏻‍♀️
-        '''
-        msg = MessagingResponse()
-        msg.message(answer)
-        print(answer)
-        print(msg)
-    
-        return str(msg)
-    
-    if "topic sequence" == incoming_msg.lower() or "what is topic sequence" == incoming_msg.lower():
-        answer = '''
-            - Topic Sequences \n
-            - Objectives \n
-            - Content \n
-            - Essential Essential Questions \n
-            - Identify a problem to solve\n
-            - Resources \n
-        '''
-        msg = MessagingResponse()
-        msg.message(answer)
-        print(answer)
-        print(msg)
-    
-        return str(msg)
-
-    if "1" == incoming_msg.lower() or "Option 1" in incoming_msg.lower():
-        answer = '''
-            You Can Choose a Topic Below 
-            Topic 1 - Idea Validation
-            Topic 2 - Business Planning
-            Topic 3 - Product Development
-            Topic 4 - User Acquisition
-            Topic 5 - Funding and Finance
-            Topic 6 - Growth and Scaling
-        '''
-        msg = MessagingResponse()
-        msg.message(answer)
-        print(answer)
-        print(msg)
-    
-        return str(msg)
-    
-    chat_log = session.get('chat_log')
-    answer = ask(incoming_msg, chat_log)
-    # session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
-                                                        #  chat_log)
+    from testingpromax import chatbotHandler
+    answer = chatbotHandler(incoming_msg)
     msg = MessagingResponse()
     msg.message(answer)
-    print(answer)
-    print(msg)
    
     return str(msg)
 
---
+++ testingpromax.py
@@ -0,0 +1,103 @@
+import sys
+import os
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import TextLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.prompts import PromptTemplate
+from langchain.chains.retrieval_qa.base import RetrievalQA
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+documents = []
+for file in os.listdir("files"):
+    if file.endswith(".pdf"):
+        pdf_path = "./files/" + file
+        loader = PyPDFLoader(pdf_path)
+        documents.extend(loader.load())
+    elif file.endswith('.txt'):
+        text_path = "./files/" + file
+        loader = TextLoader(text_path)
+        documents.extend(loader.load())
+
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
+documents = text_splitter.split_documents(documents)
+
+vectordb = Chroma.from_documents(documents, embedding=OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY), persist_directory="./data")
+vectordb.persist()
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+# llm = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0.8) , vectordb.as_retriever(), memory=memory)
+llm = ChatOpenAI(
+    model_name="gpt-3.5-turbo",
+    temperature=0,
+    openai_api_key=OPENAI_API_KEY,
+    max_tokens=512,
+)
+prompt_template = """
+
+I am athenabot, My name is athenabot.
+
+Our Purpose, We are "Building a technology startup can be an exciting and rewarding journey. 
+As a woman looking to convert your business idea into a technology business, 
+it's essential to have a clear roadmap to guide you along the way. 
+Here's our guide to help you build a technology startup 🏗️💻🚀"
+
+When User Says Hi, Ask him these options / Message:
+
+Welcome, Athenabot is here to help 1 million women entrepreneurs start and grow their technology businesses! Whether you're just starting out or looking to take your business to the next level, we've got you covered \n
+1: Learn 📚
+2: Ask a question 🙋🏻‍♀️
+
+When User Select 1st option which is learn show him these options:
+
+1 - Idea Validation
+2 - Business Planning
+3 - Product Development
+4 - User Acquisition
+5 - Funding and Finance
+6 - Growth and Scaling
+
+And Furthure Explain from the data.
+
+{context}
+
+Question: {question}"""
+SALES_PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+
+the_qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectordb.as_retriever(),
+    chain_type_kwargs={"prompt": SALES_PROMPT},
+    memory=memory
+)
+
+def chatbotHandler(message):
+    query = message
+    result = the_qa.run(query)
+    text = result
+    updated_text = text.replace("Answer", "", 1)
+    return updated_text
+
+while True:
+    query = input(f"Prompt: ")
+    if query == "exit" or query == "quit" or query == "q" or query == "f":
+        print('Exiting')
+        sys.exit()
+    if query == '':
+        continue
+    result = chatbotHandler(query)
+    print(f"Answer: " + result)
+
